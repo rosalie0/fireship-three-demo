9c841696662dffda0e0394d577697a80c48c43e1
Rosalie Newman <rosalienewman0@gmail.com>
2022-12-23 11:43:16 -0500

    torus shape rendered. it doesnt do anything

--- main.js
+++ main.js
@@ -27,3 +27,34 @@ camera.position.setZ(30); // start the camera at a position not 0,0,0 so we can 
 /*********************** CONNECTING SCENE, CAMERA, AND RENDERER **********************/
 // This actually DRAWS the two pieces we've created!
 renderer.render(scene, camera);
+
+/*********************** ******* CREATING SHAPES/ 3-D OBJECTS ********* **********************/
+
+// 1. Make Geometry
+// First get a geometry , the x, y, z points that make up a shape.
+// theres a bunch built in, such as Cylinder or TorusGeomtery
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+
+// 2. Make material
+// Again, there's many built in ( you can even make your own)
+// The MeshBasicMaterial does not require light source.
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff6666,
+  wireframe: true,
+});
+
+// 3. Make Mesh (using 1 & 2)
+// Combining the geometry with the material is the MESH - which is
+// What we actually want to add to the scene.
+const myTorus = new THREE.Mesh(geometry, material);
+
+/*********************** ******* ADDING MESHES TO SCENE ********* **********************/
+scene.add(myTorus);
+
+// Here you need to re-invoke renderer.render(scene, camera);
+// but instead, better to recursively call it
+const animate = () => {
+  requestAnimationFrame(animate); // Tell browser to repaint
+  renderer.render(scene, camera);
+};
+animate();
